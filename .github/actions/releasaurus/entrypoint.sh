30a887ca7963ba7ed674467eb483046a30076edd
Rob Gonnella <robgonnella@gmail.com>
2025-09-10 16:50:32 -0400

    ci: uses entrypoint script for releasaurus action

---
+++ .github/actions/releasaurus/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+ARGS=""
+
+if [ "${INPUT_DEBUG}" = "true" ]; then
+  ARGS="--debug"
+fi
+
+releasaurus release \
+--github-repo ${INPUT_REPO} \
+--github-token ${INPUT_TOKEN} \
+--clone-depth ${INPUT_CLONE_DEPTH} ${ARGS}
+
+releasaurus release-pr \
+--github-repo ${INPUT_REPO} \
+--github-token ${INPUT_TOKEN} \
+--clone-depth ${INPUT_CLONE_DEPTH} ${ARGS}
